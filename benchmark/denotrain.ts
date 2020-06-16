e1b22b9c49924a17ecb9a2b6a1bb5c4a78ea3639
gjuoun <gjuoun@gmail.com>
2020-06-16 15:47:27 -0300

    update benchmark scripts

---
+++ benchmark/denotrain.ts
@@ -0,0 +1,10 @@
+import { Application, Router } from "https://deno.land/x/denotrain@v0.5.0/mod.ts";
+
+const app = new Application({port: 8000});
+
+// Middleware 
+app.use((ctx) => {
+  ctx.res.body = "Hello World!"
+});
+
+await app.run();
---
+++ benchmark/drash.ts
@@ -0,0 +1,21 @@
+// File: app.ts
+
+import { Drash } from "https://deno.land/x/drash@v1.0.5/mod.ts";
+
+class HomeResource extends Drash.Http.Resource {
+  static paths = ["/"];
+  public GET() {
+    this.response.body = "Hello World!";
+    return this.response;
+  }
+}
+
+const server = new Drash.Http.Server({
+  response_output: "text/html",
+  resources: [HomeResource]
+});
+
+server.run({
+  hostname: "localhost",
+  port: 3000
+});
